e9502ac8beeb531f5c58ff7c3d0942e55354ca5d
Stevorithee@gmail.com <Stevorithee@gmail.com>
2023-09-19 08:13:50 +0300

    square

---
+++ squarenumber.py
@@ -0,0 +1,10 @@
+def main():
+    number = int(input("what is n? "))
+    print("square is ", square(number))
+
+
+def square(n):
+    return n * n
+
+
+main()   
---
+++ squareroots.py
@@ -0,0 +1,10 @@
+def main():
+    number = int(input("What's is n? "))
+    print("squared is", square(number))
+
+
+def square(n):
+    return n * n
+
+
+main()
